9fd2e0f2d1481e3c7e5d18b5d496cf1428108224
CQiiiiii <dashdfjk@outlook.com>
2026-02-07 21:11:49 +0800

    initialize

--- C/include/hashmap.h
+++ C/include/hashmap.h
@@ -5,8 +5,8 @@
 
 //基于链地址法实现
 
-typedef void *(*value_dup_fn)(const void *);
-typedef void (*value_free_fn)(void *);
+typedef void *(*value_dup_hash)(const void *);
+typedef void (*value_free_hash)(void *);
 
 typedef struct hashnode{
     char *key;
@@ -21,15 +21,14 @@ typedef struct hashmap{
     int capacity;
     int size;
     float load_factor;
-    value_free_fn free_value;
-    value_dup_fn dup_value;
+    value_free_hash free_value;
+    value_dup_hash dup_value;
 }hashmap;
 
 static void* hash_duplicate_or_assign(hashmap *map, void *value);
 hashnode *creat_Hashnode(const char *key, void *value);
-bool hashmap_set_free_dup(hashmap *map, value_dup_fn dup_, value_free_fn free_);
 uint hash_func(const char *key, int capacity);
-hashmap *hashmap_create(int initial_capacity, value_dup_fn dup_, value_free_fn free_);
+hashmap *hashmap_create(int initial_capacity, value_dup_hash dup_, value_free_hash free_);
 bool hashmap_put(hashmap *map, const char *key, void *value);
 bool hashmap_get(hashmap *map, const char *key, void **value);
 bool hashmap_remove(hashmap *map, const char *key);
